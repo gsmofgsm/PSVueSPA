70521bac4622e798edcca8840f4c0b721de86192
Qing Ma <gsmofgsm@hotmail.com>
2020-11-23 13:47:19 +0100

    scoped style

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -16,4 +16,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  padding-bottom: 40px;
+  height: 100%;
+}
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+}
+</style>

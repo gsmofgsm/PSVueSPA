e0d8e83c3243422c985b1e26b98094dca950030b
Qing Ma <gsmofgsm@hotmail.com>
2020-11-23 22:37:42 +0100

    authentication call

--- src/app.service.js
+++ src/app.service.js
@@ -12,6 +12,18 @@ const appService = {
         });
     });
   },
+  login(credentials) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post("/services/auth.php", credentials)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((response) => {
+          reject(response.status);
+        });
+    });
+  },
 };
 
 export default appService;
--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -8,7 +8,12 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <input class="input" type="text" placeholder="Your username" />
+            <input
+              v-model="username"
+              class="input"
+              type="text"
+              placeholder="Your username"
+            />
           </div>
         </div>
       </div>
@@ -20,7 +25,12 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <input class="input" type="password" placeholder="Your password" />
+            <input
+              v-model="password"
+              class="input"
+              type="password"
+              placeholder="Your password"
+            />
           </div>
         </div>
       </div>
@@ -32,10 +42,32 @@
       <div class="field-body">
         <div class="field">
           <div class="control">
-            <button class="button is-primary">Login</button>
+            <button @click="login()" class="button is-primary">Login</button>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import appService from "../app.service";
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      appService
+        .login({ username: this.username, password: this.password })
+        .then((data) => {
+          window.localStorage.setItem("token", data.token);
+          window.localStorage.setItem("tokenExpiration", data.expiration);
+        })
+        .catch(() => window.alert("Could not login!"));
+    },
+  },
+};
+</script>

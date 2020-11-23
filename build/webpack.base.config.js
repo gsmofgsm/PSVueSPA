df5e837d50333b2a0736e7181a08ee2c9be396d0
Qing Ma <gsmofgsm@hotmail.com>
2020-11-23 12:27:21 +0100

    add handle .vue files

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,20 +4,19 @@ const config = {
   entry: {
     app: path.resolve(__dirname, "../src/client-entry.js"),
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       enforce: "pre",
-  //       test: /(\.js$)/,
-  //       loader: "eslint-loader",
-  //       exclude: /node_modules/,
-  //     },
-  //   ],
-  // },
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.js",
-    },
+  module: {
+    rules: [
+      // {
+      //   enforce: "pre",
+      //   test: /(\.js$)|(\.vue$)/,
+      //   loader: "eslint-loader",
+      //   exclude: /node_modules/,
+      // },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
   },
   output: {
     path: path.resolve(__dirname, "../dist"),

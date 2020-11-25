1a3daf1e3c26455f1127abcf3431118c3010dc32
Qing Ma <gsmofgsm@hotmail.com>
2020-11-25 23:45:45 +0100

    test Post.vue with karma mocha sinon

---
+++ test/unit/specs/Post.spec.js
@@ -0,0 +1,18 @@
+import { expect } from "chai";
+import Vue from "vue";
+import Post from "../../../src/theme/Post.vue";
+
+describe("Post.vue", () => {
+  it("should render the link", () => {
+    const PostConstructor = Vue.extend(Post);
+    const comp = new PostConstructor({
+      propsData: {
+        link: "http://www.pluralsight.com",
+      },
+    }).$mount();
+    // console.log(comp);
+    expect(
+      comp.$el.querySelector(".card-footer-item").getAttribute("href")
+    ).to.equal("http://www.pluralsight.com");
+  });
+});
